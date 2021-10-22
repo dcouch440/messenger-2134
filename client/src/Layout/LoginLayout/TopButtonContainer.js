4da7373cf4eba04c6c646aa11f41194c6fab2ad3
dcouch440 <dcouch440@gmail.com>
2021-10-22 11:03:11 -0700

    add styles, prop types

--- client/src/Layout/LoginLayout/TopButtonContainer.js
+++ client/src/Layout/LoginLayout/TopButtonContainer.js
@@ -1,15 +1,32 @@
 import { Container, makeStyles } from "@material-ui/core";
 
+import PropTypes from "prop-types";
 import React from "react";
 
-const useStyles = makeStyles(() => ({
-  root: {
-    display: "flex",
-    justifyContent: "flex-end",
-    paddingTop: "35px",
-    paddingRight: "50px",
-  },
-}));
+const useStyles = makeStyles((theme) => {
+  const xsScreen = theme.breakpoints.down("xs");
+  const smlScreens = theme.breakpoints.down("sm");
+  const lgScreen = theme.breakpoints.up("lg");
+
+  return {
+    root: {
+      display: "flex",
+      justifyContent: "flex-end",
+      paddingTop: "35px",
+      paddingRight: "25px",
+      [lgScreen]: {
+        paddingTop: "65px",
+        paddingRight: "25px",
+      },
+      [smlScreens]: {
+        paddingRight: "0",
+      },
+      [xsScreen]: {
+        justifyContent: "center",
+      },
+    },
+  };
+});
 
 /**
  * @description TopButtonContainer renders children in a flex container positioned to the right.
@@ -23,6 +40,13 @@ const TopButtonContainer = ({ children }) => {
       {children}
     </Container>
   );
+};
+
+TopButtonContainer.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]).isRequired,
 };
 
 export default TopButtonContainer;
