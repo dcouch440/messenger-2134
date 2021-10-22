9c0f3ec24e7468ecdcac32fe95877148169b16e5
dcouch440 <dcouch440@gmail.com>
2021-10-22 11:00:55 -0700

    add styles

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,19 +1,50 @@
-import { Button, Grid, makeStyles } from "@material-ui/core";
-import { LoginLayout, TopButtonContainer } from "./Layout/LoginLayout";
+import { Box, makeStyles } from "@material-ui/core";
+import {
+  InputContainer,
+  LoginLayout,
+  TopButtonContainer,
+} from "./Layout/LoginLayout";
+import { LoginButton, LoginHeader, LoginInput } from "./components/Login";
 
 import ChangeRouteButton from "./components/ChangeRouteButton";
-import LoginInput from "./components/LoginInput";
 import React from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { login } from "./store/utils/thunkCreators";
 
-const useStyles = makeStyles(() => ({
-  root: {
-    width: "100%",
-    height: "100%",
-  },
-}));
+const useStyles = makeStyles((theme) => {
+  const lgScreen = theme.breakpoints.up("lg");
+
+  return {
+    root: {
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+    },
+    innerInputContainer: {
+      display: "flex",
+      flexDirection: "column",
+      height: "358px",
+      justifyContent: "space-between",
+      width: "fit-content",
+      [lgScreen]: {
+        paddingTop: "20px",
+      },
+    },
+    buttonContainer: {
+      display: "flex",
+      flexDirection: "column",
+      width: "fit-content",
+    },
+    form: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
+      flex: 1,
+    },
+  };
+});
 
 const Login = (props) => {
   const classes = useStyles();
@@ -33,7 +64,7 @@ const Login = (props) => {
 
   return (
     <LoginLayout>
-      <Grid className={classes.root}>
+      <Box className={classes.root}>
         <TopButtonContainer>
           <ChangeRouteButton
             route="/register"
@@ -41,33 +72,37 @@ const Login = (props) => {
             buttonText="Create account"
           />
         </TopButtonContainer>
-        <form onSubmit={handleLogin}>
-          {/*
-            example is currently E-mail
-          */}
-          <Grid>
-            <LoginInput
-              ariaLabel="username"
-              label="Username"
-              name="username"
-              type="text"
-              required={true}
-            />
-            <LoginInput
-              label="Password"
-              ariaLabel="password"
-              type="password"
-              name="password"
-              required={true}
-            />
-            <Grid>
-              <Button type="submit" variant="contained" size="large">
-                Login
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </Grid>
+        <InputContainer>
+          <Box className={classes.innerInputContainer}>
+            <form className={classes.form} onSubmit={handleLogin}>
+              {/*  example is currently E-mail, project expects Username */}
+              <LoginHeader text="Welcome Back!" />
+              <div>
+                <LoginInput
+                  ariaLabel="username"
+                  label="Username"
+                  name="username"
+                  type="text"
+                  required
+                />
+                <LoginInput
+                  label="Password"
+                  ariaLabel="password"
+                  type="password"
+                  name="password"
+                  required
+                />
+              </div>
+              <LoginButton
+                type="submit"
+                variant="contained"
+                size="large"
+                text="Login"
+              />
+            </form>
+          </Box>
+        </InputContainer>
+      </Box>
     </LoginLayout>
   );
 };
