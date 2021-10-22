c6bbe429d3ffece7a3e96ec81f5c773156cf2ef4
dcouch440 <dcouch440@gmail.com>
2021-10-22 11:02:43 -0700

    add export

--- client/src/Layout/LoginLayout/index.js
+++ client/src/Layout/LoginLayout/index.js
@@ -1,2 +1,3 @@
 export { default as LoginLayout } from "./LoginLayout";
 export { default as TopButtonContainer } from "./TopButtonContainer";
+export { default as InputContainer } from "./InputContainer";
