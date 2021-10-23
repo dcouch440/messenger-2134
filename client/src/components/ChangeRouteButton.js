94f37cbf9d027d302c05f6e6205b0c20c509b6c7
dcouch440 <dcouch440@gmail.com>
2021-10-23 12:21:42 -0700

    remove font-family

--- client/src/components/ChangeRouteButton.js
+++ client/src/components/ChangeRouteButton.js
@@ -37,7 +37,6 @@ const useStyles = makeStyles((theme) => {
       boxShadow: "0px 2px 12px rgba(74,106,149,0.2)",
       borderRadius: "5px",
       color: theme.colors.lightBlue,
-      fontFamily: theme.fonts.monoserrat,
       "&:hover": {
         backgroundColor: "#FFFFFF10",
       },
