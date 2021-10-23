4f3354554d3812bb9b5c283abcc2e684c2e6194d
dcouch440 <dcouch440@gmail.com>
2021-10-23 11:10:25 -0700

    add exports

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,3 +1,6 @@
 export { default as LoginButton } from "./LoginButton";
 export { default as LoginHeader } from "./LoginHeader";
 export { default as LoginInput } from "./LoginInput";
+export { default as LoginForm } from "./LoginForm";
+export { default as LoginBubbleMobile } from "./LoginBubbleMobile";
+export { default as LoginSideBar } from "./LoginSideBar";
