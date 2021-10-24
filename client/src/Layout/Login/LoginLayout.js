abd4428a77be8311f5c0c29b3fe545eb02d4f142
dcouch440 <dcouch440@gmail.com>
2021-10-24 12:22:12 -0700

    remove column reverse

--- client/src/Layout/Login/LoginLayout.js
+++ client/src/Layout/Login/LoginLayout.js
@@ -6,7 +6,6 @@ import React from "react";
 
 const useStyles = makeStyles((theme) => {
   const xsScreen = theme.breakpoints.down("xs");
-  // const smlScreen = theme.breakpoints.down("sm");
 
   return {
     root: {
@@ -18,10 +17,10 @@ const useStyles = makeStyles((theme) => {
     childrenOuter: {
       height: "100%",
       width: "100%",
-      flex: 1,
+      flex: "1",
       [xsScreen]: {
-        paddingRight: 0,
-        paddingLeft: 0,
+        paddingRight: "0",
+        paddingLeft: "0",
       },
     },
     children: {
@@ -29,17 +28,12 @@ const useStyles = makeStyles((theme) => {
       height: "100%",
       display: "flex",
       flexDirection: "column",
-      [xsScreen]: {
-        flexDirection: "column-reverse",
-      },
     },
   };
 });
 
 /**
- *
- * @description LoginLayout renders child components in with a side banner.
- *
+ * @description LoginLayout component is a Material-UI component that renders child components with a side banner.
  */
 
 const LoginLayout = ({ children }) => {
