b83fead2e6824a887a53ca8966ff17057a385720
dcouch440 <dcouch440@gmail.com>
2021-10-24 12:20:17 -0700

    add mobile route button

--- client/src/components/Login/LoginForm.js
+++ client/src/components/Login/LoginForm.js
@@ -1,26 +1,34 @@
 import { Box, makeStyles } from "@material-ui/core";
 import { LoginBubbleMobile, LoginButton, LoginHeader, LoginInput } from ".";
 
+import { ChangeRouteButton } from "..";
 import PropTypes from "prop-types";
 import React from "react";
 import { useState } from "react";
 
-const useStyles = makeStyles(() => {
+const useStyles = makeStyles((theme) => {
+  const xsScreen = theme.breakpoints.down("sm");
+
   return {
     root: {
       display: "flex",
       flexDirection: "column",
       justifyContent: "space-between",
-      flex: 1,
+      flex: "1",
     },
-    bubble: {
-      paddingBottom: "52px",
+    mobileRouteButton: {
+      display: "none",
+      [xsScreen]: {
+        display: "flex",
+        width: "100%",
+        marginTop: "25px",
+      },
     },
   };
 });
 
 /**
- * @description LoginForm component is a Material-UI component that takes username and password.
+ * @description LoginForm component is a Material-UI form that takes username and password - does not prevent default.
  */
 
 const LoginForm = ({ onSubmit }) => {
@@ -43,24 +51,28 @@ const LoginForm = ({ onSubmit }) => {
   return (
     <form className={classes.root} onSubmit={handleSubmit}>
       {/* shows on mobile only */}
-      <LoginBubbleMobile className={classes.bubble} />
+      <LoginBubbleMobile />
       {/*  example is currently E-mail, project expects Username */}
       <Box>
         <LoginHeader text="Welcome Back!" />
         <LoginInput
+          className={classes.inputs}
           ariaLabel="username"
           label="Username"
           name="username"
           type="text"
+          autoComplete="username"
           onChange={handleChange}
           value={username}
           required
         />
         <LoginInput
+          className={classes.inputs}
           label="Password"
           ariaLabel="password"
           type="password"
           name="password"
+          autoComplete="password"
           onChange={handleChange}
           value={password}
           forgot
@@ -72,6 +84,13 @@ const LoginForm = ({ onSubmit }) => {
         variant="contained"
         size="large"
         text="Login"
+      />
+      <ChangeRouteButton
+        className={classes.mobileRouteButton}
+        route="/register"
+        sideText="Don't have an account?"
+        buttonText="Create account"
+        variant="text"
       />
     </form>
   );
