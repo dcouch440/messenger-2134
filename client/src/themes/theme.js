486e48dfc92fccf191ab94bf709dc7b362775929
dcouch440 <dcouch440@gmail.com>
2021-10-28 13:51:37 -0700

    revert changes

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -4,18 +4,10 @@ export const theme = createMuiTheme({
   typography: {
     fontFamily: "Open Sans, sans-serif",
     fontSize: 14,
-    fontSizeLg: 26,
-    fontSizeXL: 36,
-    fontWeightMedium: 600,
-    h2: {
-      fontWeight: 600,
-      fontSize: 26,
-    },
     button: {
       textTransform: "none",
       letterSpacing: 0,
-      fontWeight: 600,
-      fontFamily: "Montserrat, sans-serif",
+      fontWeight: "bold",
     },
   },
   overrides: {
@@ -26,13 +18,8 @@ export const theme = createMuiTheme({
     },
   },
   palette: {
-    primary: { main: "#3A8DFF", dark: "#86B9FF" },
+    primary: { main: "#3A8DFF" },
     secondary: { main: "#B0B0B0" },
-    transparentBlue: { main: "#4A6A9520" },
-    transparentWhite: { main: "#FFFFFF10" },
-    blueGradient: {
-      main: "linear-gradient(0deg, rgba(134,185,255,.85) 0%, rgba(58,141,255,.85) 100%)",
-    },
     dimGrey: { main: "#F4F6FA" },
     darkBlueGradient: {
       main: "linear-gradient(225deg, #6CC1FF 0%, #3A8DFF 100%)",
